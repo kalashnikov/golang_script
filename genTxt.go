eb4e518a75f3d4f118be6955994e540afcdb1d18
kalakuo <kalaexj@gmail.com>
2015-06-20 19:11:23 +0800

    code cleanup

--- genTxt.go
+++ genTxt.go
@@ -11,35 +11,30 @@ import (
 	"sync"
 )
 
-//const folder = "html/"
-//const out = "txt/"
+type RegPack struct {
+	re_pre    *regexp.Regexp
+	re_en     *regexp.Regexp
+	re_div    *regexp.Regexp
+	re_strong *regexp.Regexp
+	re_ruby   *regexp.Regexp
+	re_span   *regexp.Regexp
+	re_h4     *regexp.Regexp
+	re_em     *regexp.Regexp
+	re_post   *regexp.Regexp
+}
 
-func genTxt(file string,
-	re_pre *regexp.Regexp,
-	re_br *regexp.Regexp,
-	re_en *regexp.Regexp,
-	re_enn *regexp.Regexp,
-	re_div *regexp.Regexp,
-	re_strong *regexp.Regexp,
-	re_ruby *regexp.Regexp,
-	re_span *regexp.Regexp,
-	re_h4 *regexp.Regexp,
-	re_em *regexp.Regexp,
-	re_post *regexp.Regexp) {
-
+func genTxt(file string, reg RegPack) {
 	outpath := "txt/" + strings.Split(path.Base(file), ".")[0] + ".txt"
 	if f, err := ioutil.ReadFile(file); err == nil {
-		str := re_pre.ReplaceAllString(string(f), "")
-		str = re_br.ReplaceAllString(str, "")
-		str = re_div.ReplaceAllString(str, "")
-		str = re_strong.ReplaceAllString(str, "")
-		str = re_ruby.ReplaceAllString(str, "")
-		str = re_span.ReplaceAllString(str, "")
-		str = re_h4.ReplaceAllString(str, "")
-		str = re_em.ReplaceAllString(str, "")
-		str = re_post.ReplaceAllString(str, "")
-		str = re_en.ReplaceAllString(str, "")
-		str = re_enn.ReplaceAllString(str, "")
+		str := reg.re_pre.ReplaceAllString(string(f), "") // Section front of content
+		str = reg.re_post.ReplaceAllString(str, "")       // Section back of content
+		str = reg.re_div.ReplaceAllString(str, "")
+		str = reg.re_strong.ReplaceAllString(str, "")
+		str = reg.re_ruby.ReplaceAllString(str, "")
+		str = reg.re_span.ReplaceAllString(str, "")
+		str = reg.re_h4.ReplaceAllString(str, "")
+		str = reg.re_em.ReplaceAllString(str, "")
+		str = reg.re_en.ReplaceAllString(str, "") // Signle tag
 		fmt.Println(outpath)
 		if out, err := os.Create(outpath); err == nil {
 			out.WriteString(str)
@@ -48,21 +43,18 @@ func genTxt(file string,
 }
 
 func main() {
-	//file := folder + "1142_20453.html"
-	//outpath := out + strings.Split(path.Base(file), ".")[0] + ".txt"
 
 	// Regular expression for html clean-up
-	re_pre := regexp.MustCompile("(?s)^.*^.*<div class=\"main_text\">")
-	re_br := regexp.MustCompile("<br ?/?>")
-	re_en := regexp.MustCompile("<[a-z]+.*/>")
-	re_enn := regexp.MustCompile("</[a-z]+>")
-	re_div := regexp.MustCompile("(?s)<div[^>]*>(.*?)</div>")
-	re_strong := regexp.MustCompile("(?s)<strong[^>]*>(.*?)</strong>")
-	re_ruby := regexp.MustCompile("(?s)<ruby[^>]*>(.*?)</ruby>")
-	re_span := regexp.MustCompile("(?s)<span[^>]*>(.*?)</span>")
-	re_h4 := regexp.MustCompile("(?s)<h4[^>]*>(.*?)</h4>")
-	re_em := regexp.MustCompile("(?s)<em[^>]*>(.*?)</em>")
-	re_post := regexp.MustCompile("(?s)(</div>\n)?<div class=\"bibliographical_information\">.*$")
+	var reg RegPack
+	reg.re_pre = regexp.MustCompile("(?s)^.*^.*<div class=\"main_text\">")
+	reg.re_en = regexp.MustCompile("<(?i)[/|!]?[a-z]+.*/?>")
+	reg.re_div = regexp.MustCompile("(?is)<div[^>]*>(.*?)</div>")
+	reg.re_strong = regexp.MustCompile("(?is)<strong[^>]*>(.*?)</strong>")
+	reg.re_ruby = regexp.MustCompile("(?is)<ruby[^>]*>(.*?)</ruby>")
+	reg.re_span = regexp.MustCompile("(?is)<span[^>]*>(.*?)</span>")
+	reg.re_h4 = regexp.MustCompile("(?is)<h4[^>]*>(.*?)</h4>")
+	reg.re_em = regexp.MustCompile("(?is)<em[^>]*>(.*?)</em>")
+	reg.re_post = regexp.MustCompile("(?s)(</div>\n)?<div class=\"bibliographical_information\">.*$")
 
 	// Ref: How would you define a pool of goroutines to be executed at once in Golang?
 	//  http://stackoverflow.com/questions/18405023/how-would-you-define-a-pool-of-goroutines-to-be-executed-at-once-in-golang
@@ -74,7 +66,7 @@ func main() {
 		wg.Add(1)
 		go func() {
 			for file := range tasks {
-				genTxt(file, re_pre, re_br, re_en, re_enn, re_div, re_strong, re_ruby, re_span, re_h4, re_em, re_post)
+				genTxt(file, reg)
 			}
 			wg.Done()
 		}()
