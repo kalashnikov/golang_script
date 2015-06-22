6e36859192d08a86d426892ecd08f201202e726e
kalashnikov <kalaexj@gmail.com>
2015-06-22 23:59:45 +0800

    add idf part

--- mecab.go
+++ mecab.go
@@ -5,6 +5,7 @@ import (
 	"github.com/bluele/mecab-golang"
 	"golang.org/x/text/width"
 	"io/ioutil"
+	"math"
 	"path/filepath"
 	"sort"
 	"strings"
@@ -14,13 +15,14 @@ import (
 
 // https://github.com/dbravender/go_mapreduce/blob/master/src/mapreduce/mapreduce.go
 func MapReduce(mapper func(interface{}, chan interface{}),
-	reducer func(chan interface{}, chan interface{}),
+	reducer func(chan interface{}, chan interface{}, chan interface{}),
 	input chan interface{},
-	pool_size int) interface{} {
+	pool_size int) (interface{}, interface{}) {
 	reduce_input := make(chan interface{})
 	reduce_output := make(chan interface{})
+	reduce_output2 := make(chan interface{})
 	worker_output := make(chan chan interface{}, pool_size)
-	go reducer(reduce_input, reduce_output)
+	go reducer(reduce_input, reduce_output, reduce_output2)
 	go func() {
 		for worker_chan := range worker_output {
 			reduce_input <- <-worker_chan
@@ -35,7 +37,7 @@ func MapReduce(mapper func(interface{}, chan interface{}),
 		}
 		close(worker_output)
 	}()
-	return <-reduce_output
+	return <-reduce_output, <-reduce_output2
 }
 
 // https://github.com/dbravender/go_mapreduce/blob/master/src/wordcount/wordcount.go
@@ -52,25 +54,29 @@ func wordcount(filename interface{}, output chan interface{}) {
 	output <- results
 }
 
-func reducer(input chan interface{}, output chan interface{}) {
+func reducer(input chan interface{}, output chan interface{}, output2 chan interface{}) {
 	results := map[string]int{}
+	results2 := map[string]int{}
 	for new_matches := range input {
 		for key, value := range new_matches.(map[string]int) {
 			previous_count, exists := results[key]
 			if !exists {
 				results[key] = value
+				results2[key] = 1
 			} else {
 				results[key] = previous_count + value
+				results2[key] = previous_count + 1
 			}
 		}
 	}
 	output <- results
+	output2 <- results2
 }
 
 func getFiles(folder string) chan interface{} {
 	output := make(chan interface{})
 	go func() {
-		if a, err := filepath.Glob(folder + "/*.txt"); err == nil {
+		if a, err := filepath.Glob(folder); err == nil {
 			for _, file := range a {
 				output <- file
 			}
@@ -160,17 +166,50 @@ func cleanWords(ary []string, stopwords map[string]bool) []string {
 	return out
 }
 
+func calculateTFIDF(tf interface{}, idf interface{}, folder string) (map[string]float64, map[string]float64) {
+
+	tf_ := tf.(map[string]int)
+	idf_ := idf.(map[string]int)
+
+	tf_out := make(map[string]float64, len(tf_))
+	idf_out := make(map[string]float64, len(idf_))
+
+	// Get Total term/word count
+	tc := 0
+	for _, v := range tf_ {
+		tc += v
+	}
+
+	// Get Total document count
+	dc := 0
+	if a, err := filepath.Glob("txt/*.txt"); err == nil {
+		dc = len(a)
+	}
+
+	for k, v := range tf_ {
+		tf_out[k] = float64(v)
+	}
+
+	// Inverse Document Frequency = Log ( document count / Total document count )
+	for k, v := range idf_ {
+		idf_out[k] = math.Log(float64(dc) / float64(v))
+	}
+
+	return tf_out, idf_out
+}
+
 func main() {
 	start := time.Now()
 
 	// Generate candidate words
-	result := MapReduce(wordcount, reducer, getFiles("txt"), 20).(map[string]int)
+	tf_, idf_ := MapReduce(wordcount, reducer, getFiles("txt/*.txt"), 20)
+	tf, idf := calculateTFIDF(tf_, idf_, "txt/*.txt")
 
 	stopwords := getStopWords()
 
 	// Check Result
 	var keys []string
-	for k := range result {
+	for k := range tf {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
@@ -178,7 +217,7 @@ func main() {
 	words := cleanWords(keys, stopwords)
 
 	for _, v := range words {
-		fmt.Printf("%s: %d\n", v, result[v])
+		fmt.Printf("%s,%g,%g\n", v, tf[v], idf[v])
 	}
 	fmt.Printf("Time used: %v", time.Since(start))
 }
