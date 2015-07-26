267a93c4fab1174562052b21e4d27eae033a3a87
kalashnikov <kalaexj@gmail.com>
2015-07-27 01:38:57 +0800

    Update

--- mecab2.go
+++ mecab2.go
@@ -7,12 +7,13 @@ import (
 	"github.com/bluele/mecab-golang"
 	"golang.org/x/text/width"
 	"io/ioutil"
-	"log"
 	"os"
 	"path"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"runtime/pprof"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -24,39 +25,69 @@ var mutex = &sync.Mutex{}
 func calTF(fname string,
 	idfList map[string]float64,
 	tfList map[string]map[string]float64,
+	fnameList map[int]string,
 	stopwords map[string]bool) {
 
 	results := map[string]float64{}
 
 	file, err := os.Open(fname)
 	if err != nil {
-		log.Fatal(err)
+		panic(fname)
 	}
 	defer file.Close()
 
-	r := bufio.NewReader(file)
-	s, e := Readln(r)
-	for e == nil {
-		for _, v := range parseToNode(s) {
+	// Load the contents and pass into tokenizer
+	// ReadFile is faster but memory consuming
+	// Readln (read line-by-line) will be 10x slower
+	if f, err := ioutil.ReadFile(fname); err == nil {
+		for _, v := range parseToNode(string(f)) {
 			if v == "" {
 				continue
-			} else if str, isWord := filter(v); str != "" && isWord && !stopwords[str] {
+			} else if str, isWord := filter(v); str != "" && isWord && stopwords[str] {
 				continue
 			}
 			results[v]++
 		}
-		s, e = Readln(r)
 	}
 
-	file_id := ""
-	if strings.Contains(fname, "_") {
-		file_id = strings.Split(path.Base(fname), "_")[0]
+	// Get author and title
+	file_id, author, title := "", "", ""
+	if strings.Contains(fname, "book_txt") {
+		author, title = GetTitle(path.Base(fname))
+		id := len(fnameList) + 1000001
+		file_id = strconv.Itoa(id)
 	} else {
-		file_id = strings.Split(path.Base(fname), ".")[0]
+		r := bufio.NewReader(file)
+		s, _ := Readln(r)
+		if strings.Contains(s, " ") {
+			ary := strings.Split(s, " ")
+			author, title = ary[0], ary[1]
+		} else {
+			title = s
+		}
+
+		// File id for aozora
+		if strings.Contains(fname, "_") {
+			file_id = strings.Split(path.Base(fname), "_")[0]
+		} else {
+			file_id = strings.Split(path.Base(fname), ".")[0]
+		}
+	}
+	fmt.Printf("# Fin: %s - %s | %s\n", author, title, fname)
+
+	// Add weighting to author & title
+	for _, v := range parseToNode(author + " " + title) {
+		if v == "" {
+			continue
+		} else if str, isWord := filter(v); str != "" && isWord && !stopwords[str] {
+			continue
+		}
+		results[v] += 10
 	}
 
 	tfList[file_id] = results
 
+	// Add into IDF list
 	for v := range results {
 		mutex.Lock()
 		if _, ok := idfList[v]; ok {
@@ -64,6 +95,8 @@ func calTF(fname string,
 		} else {
 			idfList[v] = 1.0
 		}
+		id, _ := strconv.Atoi(file_id)
+		fnameList[id] = fname
 		mutex.Unlock()
 	}
 }
@@ -78,34 +111,49 @@ func main() {
 	if *cpuprofile != "" {
 		f, err := os.Create(*cpuprofile)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
 		pprof.StartCPUProfile(f)
 		defer pprof.StopCPUProfile()
 	}
+
+	searchStr := "*txt/*.txt"
+	//searchStr := "book_txt/*.txt"
 
 	// Filter out symbol and stopwords
 	stopwords := getStopWords()
 
 	tfList := make(map[string]map[string]float64, 20000)
 	idfList := make(map[string]float64, 220000)
+	fnameList := make(map[int]string, 20000)
 
 	// spawn four worker goroutines
 	fmt.Println("### calTF ###")
+	tasks := make(chan string, 64)
 	var wg sync.WaitGroup
 	for i := 0; i < cpunum; i++ {
 		wg.Add(1)
 		go func() {
-			for file := range getFiles("*txt/*.txt") {
-				calTF(file.(string), idfList, tfList, stopwords)
+			for file := range tasks {
+				calTF(file, idfList, tfList, fnameList, stopwords)
 			}
 			wg.Done()
 		}()
 	}
+
+	if a, err := filepath.Glob(searchStr); err == nil {
+		for _, f := range a {
+			if f == "" {
+				continue
+			}
+			tasks <- string(f)
+		}
+	}
+	close(tasks)
 	wg.Wait()
 
 	fmt.Println("### Refine IDF ###")
-	if a, err := filepath.Glob("*txt/*.txt"); err == nil {
+	if a, err := filepath.Glob(searchStr); err == nil {
 		total_doc := float64(len(a))
 		for _, v := range idfList {
 			v /= total_doc
@@ -136,14 +184,16 @@ func main() {
 			if word == "" || strings.Contains(word, ",") {
 				continue
 			}
-			if idf, ok := tfList[word]; ok {
+			if idf, ok := idfList[word]; ok {
 				str := fmt.Sprintf("%s,%g,", word, idf)
 				list := make([]string, 0, len(m))
 				for doc, v := range m {
 					list = append(list, fmt.Sprintf("%s_%g", doc, v))
 				}
-				str = fmt.Sprintf("%s%s\n", str, strings.Join(list, ","))
-				out.WriteString(str)
+				fstr := fmt.Sprintf("%s%s\n", str, strings.Join(list, ","))
+				out.WriteString(fstr)
+			} else {
+				fmt.Printf("### ERROR - idfList not found: %s\n", word)
 			}
 		}
 	}
@@ -153,6 +203,15 @@ func main() {
 	if out, err := os.Create(outputFile2); err == nil {
 		for w, v := range idfList {
 			str := fmt.Sprintf("%s,%g\n", w, v)
+			out.WriteString(str)
+		}
+	}
+
+	fmt.Println("### Write fname list ###")
+	outputFile3 := "fnameList.csv"
+	if out, err := os.Create(outputFile3); err == nil {
+		for w, v := range fnameList {
+			str := fmt.Sprintf("%d,%s\n", w, v)
 			out.WriteString(str)
 		}
 	}
@@ -256,4 +315,34 @@ func Readln(r *bufio.Reader) (string, error) {
 		ln = append(ln, line...)
 	}
 	return string(ln), err
+}
+
+func GetTitle(file string) (string, string) {
+	re1 := regexp.MustCompile(".txt")
+	re2 := regexp.MustCompile("(校正.*)")
+
+	fname := re1.ReplaceAllString(file, "")
+	str := re2.ReplaceAllString(fname, " ")
+
+	author, title := "", ""
+	if strings.Contains(str, "－") {
+		ary := strings.Split(str, "－")
+		author, title = ary[0], ary[1]
+	} else if strings.Contains(str, "-") {
+		ary := strings.Split(str, "-")
+		author, title = ary[0], ary[1]
+	} else if strings.Contains(str, " ") {
+		idx := strings.Index(str, " ")
+		author, title = str[0:idx-1], str[idx+1:]
+	} else if strings.Contains(str, "_") {
+		idx := strings.LastIndex(str, "_")
+		author, title = str[0:idx-1], str[idx+1:]
+	} else {
+		panic(str)
+	}
+
+	if strings.Contains(title, "(") {
+		title = title[0 : len(title)-2]
+	}
+	return author, title
 }
